081a091cae70e70e14922c4e542c87f16126707f
Nihal SANDHU <nihalsandhu007220@gmail.com>
2025-11-15 17:35:50 +0530

    exploring get post delete patch

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("hello from dasboard");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "nihal", lastName: "Sandhu" });
+});
+app.post("/user", (req, res) => {
+  console.log("save data to database");
+  res.send("data saved succesfullt to the database");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("hello helo hello  ");
-});
-app.use("/test", (req, res) => {
-  res.send("hello from the server ");
+app.delete("/user", (req, res) => {
+  res.send("user deleted succefully");
 });
 
+app.patch("/user", (req, res) => {
+  res.send("user updated succesfully");
+});
+app.put("/user", (req, res) => {
+  res.send("user whole data changed");
+});
 app.listen(3000, () => console.log("Server started"));
-grgrgr;
