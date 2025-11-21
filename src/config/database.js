dcd8957ad9630ab25dff4e1f903fc49686d9faa6
Nihal Sandhu <121277127+nihalsandhu09@users.noreply.github.com>
2025-11-21 13:14:36 +0530

    Delete src/config/database.js

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://nihalsandhu:Nihal12345@nodejs.ciembsu.mongodb.net/devTinder"
-  );
-};
-
-module.exports = connectDB;
